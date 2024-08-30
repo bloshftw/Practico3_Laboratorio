824b4695fbdf17095182fc3efad2e025cb4b3065
Luciano Jesus Rodriguez <luciano_rodriguez86@hotmail.com>
2024-08-30 19:51:05 -0300

    Ej1 terminado
    queda resolver ejercicio2

--- src/main/java/com/mycompany/practico3_laboratorio/Practico3_Laboratorio.java
+++ src/main/java/com/mycompany/practico3_laboratorio/Practico3_Laboratorio.java
@@ -1,16 +1,17 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package com.mycompany.practico3_laboratorio;
 
-/**
- *
- * @author Lucia
- */
+
 public class Practico3_Laboratorio {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Pantalla app = new Pantalla();
+        
+        /*Centra la ventana al medio*/
+        app.setLocationRelativeTo(null);
+        
+        app.setTitle("Ingrese las credenciales");
+        app.setVisible(true);
+        
+        
     }
 }
