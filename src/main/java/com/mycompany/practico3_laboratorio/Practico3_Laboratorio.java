cc16bb6301cdffa0feb668ededa6007e0231ad14
Luciano Jesus Rodriguez <luciano_rodriguez86@hotmail.com>
2024-08-30 16:38:31 -0300

    Initial commit

---
+++ src/main/java/com/mycompany/practico3_laboratorio/Practico3_Laboratorio.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.practico3_laboratorio;
+
+/**
+ *
+ * @author Lucia
+ */
+public class Practico3_Laboratorio {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
